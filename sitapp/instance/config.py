234a42a5c65c495dc00abb3de65c87b7d63990fb
Changbeom Choi <cbchoi@handong.edu>
2019-11-28 10:05:35 +0000

    update

--- sitapp/instance/config.py
+++
@@ -1,9 +0,0 @@
-MAIL_SERVER='smtp.googlemail.com'
-MAIL_PORT=587
-MAIL_USE_TLS=True
-MAIL_USERNAME='cbchoi@handong.edu'
-MAIL_PASSWORD='iyxddxxprbkyretc'
-MAIL_SUBJECT_PREFIX='SIT32006'
-MAIL_SENDER='cbchoi@handong.edu'
-
-ADMIN="cbchoi@handong.edu"
