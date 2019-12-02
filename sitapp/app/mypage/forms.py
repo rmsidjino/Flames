92a8fa4d663bfa5f3c6011ca8e03c3d2842b84e2
rmsidjino <21300704@handong.edu>
2019-12-02 22:12:26 +0000

    mypage

--- sitapp/app/mypage/forms.py
+++ sitapp/app/mypage/forms.py
@@ -1,28 +1,43 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Email, Length, Regexp, EqualTo
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import Required, Email, Length
 
-from wtforms import ValidationError
-from ..models import User
+# 20191122
+from wtforms import SelectField, ValidationError
+from wtforms.validators import Regexp, EqualTo
+
 from .. import db
 
-class LoginForm(FlaskForm):
-	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
-	password = PasswordField('Password', validators=[Required()])
-	remember_me = BooleanField('Keep me logged in')
-	submit = SubmitField('Log In')
+######
+class SearchitemForm(FlaskForm):    
+    name = StringField('What item are you interested in?', validators=[Required()])    
+    submit = SubmitField('Search') 
+######
 
+# 20191122
+class EditProfileForm(FlaskForm):
+	username = StringField('Real name', validators=[Length(0, 64)])
+	submit = SubmitField('Submit')
 
-class RegistrationForm(FlaskForm):
-	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
-	username = StringField('Username', validators=[Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-							'Usernames must have only letters, '
-							'numbers, dots or underscores')])
-	password = PasswordField('Password', validators=[Required(), 
-													EqualTo('password2', 
-													message='Passwords must match.')])
-	password2 = PasswordField('Confirm password', validators=[Required()])
-	submit = SubmitField('Register')
+class EditProfileAdminForm(FlaskForm):
+	id = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+	username = StringField('Username', validators=[Required(), Length(1, 64), 
+		Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usernames must have only letters, ''numbers, dots or underscores')])
+	confirmed = BooleanField('Confirmed')
+	role = SelectField('Role')
+	username = StringField('Real name', validators=[Length(0, 64)])
+	submit = SubmitField('Submit')
+
+	def __init__(self, user, *args, **kwargs):
+		super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+		collection = db.get_collection('roles')
+		results = collection.find({ } , { "name": True })
+		
+		lst = [result['name'] for result in results] 
+		r_lst = [(role, role) for role in lst]
+
+		self.role.choices = r_lst
+		self.user = user
 
 	def validate_email(self, field):
 		collection = db.get_collection('users')
@@ -30,10 +45,30 @@ class RegistrationForm(FlaskForm):
 		if results is not None:
 			raise ValidationError('Email already registered.')
 		pass
-	
-	def validate_username(self, field):
+
+class RegisterItemAdminForm(FlaskForm):
+	iid = StringField('iname', validators=[Length(0, 64)])
+	username = StringField('Username', validators=[Required(), Length(1, 64), 
+		Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usernames must have only letters, ''numbers, dots or underscores')])
+	confirmed = BooleanField('Confirmed')
+	role = SelectField('Role')
+	username = StringField('Real name', validators=[Length(0, 64)])
+	submit = SubmitField('Submit')
+
+	def __init__(self, user, *args, **kwargs):
+		super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+		collection = db.get_collection('roles')
+		results = collection.find({ } , { "name": True })
+		
+		lst = [result['name'] for result in results] 
+		r_lst = [(role, role) for role in lst]
+
+		self.role.choices = r_lst
+		self.user = user
+
+	def validate_email(self, field):
 		collection = db.get_collection('users')
-		results = collection.find_one({'username':field.data})
+		results = collection.find_one({'id':field.data})
 		if results is not None:
-			raise ValidationError('Username already registered.')
+			raise ValidationError('Email already registered.')
 		pass
--- sitapp/app/mypage/views.py
+++ sitapp/app/mypage/views.py
@@ -1,14 +1,49 @@
-from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, login_required, logout_user
-from ..models import User
-from .forms import LoginForm, RegistrationForm
+from datetime import datetime
+from flask import render_template, session, redirect, url_for, flash,request
+
 from . import mypage
+from .forms import SearchitemForm
 
+from ..models import User, Permission
+
+from flask_login import login_user, login_required, logout_user
+from ..decorators import admin_required, permission_required
+
+# 20191122
 from .. import db
-from ..email import send_email # added 20191108
-
+from .forms import EditProfileForm, EditProfileAdminForm
 from flask_login import current_user
 
+from werkzeug import secure_filename
+from .. import fs
+from flask import send_file
+
+
 @mypage.route('/', methods=['GET', 'POST'])
-def mypage_main():
-    return "Mypage"
+def index():
+	return render_template('mypage.html',
+							item_list = [i for i in db.get_collection('items').find()],
+							file_lst = {file:url_for('main.image', filename=file) for file in fs.list()},
+							name=session.get('name'),
+							known=session.get('known', False),
+							current_time=datetime.utcnow())
+# 20191122
+
+
+#@main.route('/search_hash')
+#def search_hash(hid):
+#     col_item = db.get_collection('hash_map')
+#     results = col_item.find({'hid':hid})
+#     return render_template('') ##
+
+
+#def search(iid):
+#     col_item = db.get_collection('item')
+#     results = col_item.find({'hash_map':})
+#     return render_template() ##
+
+
+@mypage.route('/images/<filename>')
+def image(filename):
+    gridout = fs.get_last_version(filename=filename)
+    return send_file(gridout, mimetype=gridout.content_type)
