fbb9560e7a847d43b17c8acaad2f225f865059e6
sinyongkim <21600824@handong.edu>
2019-12-04 05:17:54 +0000

    test_web

--- web/manage.py
+++ web/manage.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import os
 from app import create_app
-from app.models import User, Role # 20191112
+from app.models import User, Role 
 from flask_script import Manager, Shell, Server
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 
 def make_shell_context():
-	return dict(app=app, User=User, Role=Role) # 20191112
+	return dict(app=app, User=User, Role=Role)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("runserver", Server(host='0.0.0.0', port=5000))
