0fc9d20883ddbfbe60a03f71968055b0b9083e8f
sinyongkim <21600824@handong.edu>
2019-12-04 05:16:33 +0000

    test

--- test/flames.py
+++ test/flames.py
@@ -1,72 +1,232 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(object):
+from . import login_manager, db
+
+
+from flask import current_app, request, url_for
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer 
+
+from flask_login import AnonymousUserMixin
+
+
+from datetime import datetime
+
+class User(UserMixin, object):
+	id = ""
 	username = ""
-	ID = ""
-	password = ""
-	#permission = 0
+	role = None
+	password_hash = ""
+	confirmed = False
+	member_since = ""
+	last_seen = ""
+
+	def __init__(self, email, username, password):
+		self.id = email
+		self.username = username
+		self.password = password
+
+		collection = db.get_collection('roles')
+		if self.id == current_app.config['ADMIN']:
+			self.role = Role('Administrator', 0xff)
+		else:
+			result = collection.find_one({'default':True})
+			self.role = Role(result['name'], result['permission'], result['default'])
+		
+		 
+	@property
+	def password(self):
+		raise AttributeError('password is not a readable attribute')
+	 
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+	
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
+	@login_manager.user_loader
+	def load_user(user_id):
+		collection = db.get_collection('users')
+		results = collection.find_one({'id':user_id})
+		if results is not None:
+			user = User(results['id'], "", "") 
+			user.from_dict(results)
+			return user
+		else:
+			return None
+
+	
+	def generate_confirmation_token(self, expiration=3600):
+		s = Serializer(current_app.config['SECRET_KEY'], expiration)
+		return s.dumps({'confirm': self.id}).decode('utf-8')
+
+	def confirm(self, token):
+		s = Serializer(current_app.config['SECRET_KEY'])
+		try:
+			data = s.loads(token.encode('utf-8'))
+		except:
+			return False
+		if data.get('confirm') != self.id:
+			return False
+		self.confirmed = True
+		collection = db.get_collection('users')
+		results = collection.update_one({'id':self.id}, {'$set':{'confirmed':self.confirmed}})
+		return True
+    
+
+	
+    
+	def can(self, permissions):
+		return self.role is not None and (self.role.permission & permissions) == permissions
+    
+	def is_administrator(self):
+		return self.can(Permission.ADMINISTATOR)
 	
 
-	def __init__(self, username, ID, password, default=False):
-		User.username = username
-		User.ID = ID
-		User.password = password
-		#Flames.permission = permission
-		User.default = default
-				
+	
+	
+	def ping(self):
+		self.last_seen = datetime.utcnow()
+		collection = db.get_collection('users')
+		results = collection.update_one({'id':self.id}, {'$set':{'last_seen':self.last_seen}})
+
 
 	def to_dict(self):
-		dict_flames = {
-			'name': self.name, 
-			'ID': self.ID,
-			'passwd': self.password,
-		#	'permission': self.permission,
-			'default': self.default
+		dict_user = {
+			'id': self.id, 
+			'username':self.username,
+			
+			'role_id':self.role.name,
+			'role_permission':self.role.permission,
+			
+			'password_hash':self.password_hash,
+			'confirmed':self.confirmed,
+			
+			'member_since':self.member_since,
+			'last_seen':self.last_seen
 		}
-		return dict_flames
+		return dict_user
 
 	def from_dict(self, data):
 		if data is not None:
-			self.name = data['name']
-			self.ID = data['ID']
-			self.password = data['password']
-		#	self.permission = data['permission']
-			self.default = data['default']
+			self.id = data['id']
+			self.username = data['username']
+			
+			self.role = Role(data['role_id'], data['role_permission'])
+			
+			self.password_hash = data['password_hash']
+			self.confirmed = data['confirmed']
+			
+			self.member_since = data.get('member_since')
+			self.last_seen = data.get('last_seen')
 
-class Item(object):
+
+
+class AnonymousUser(AnonymousUserMixin):
+	def can(self, permissions):
+		return False
+    
+	def is_administrator(self):
+		return False
+
+class Role(object):
 	name = ""
-	ID = ""
-	price = ""
-	picture = ""
-	#permission = 0
-	
+	permission = 0
+	default = False
 
-	def __init__(self, name, ID, price, picture, default=False):
-		Flames.name = name
-		Flames.ID = ID
-		Flames.price = price
-		Flames.picture = picture
-		#Flames.permission = permission
-		Flames.default = default
+	def __init__(self, name, permission, default=False):
+		Role.name = name
+		Role.permission = permission
+		Role.default = default
+
+	@staticmethod
+	def insert_roles():
+		roles = {
+		'User': (Permission.FOLLOW |
+				 Permission.COMMENT |
+				 Permission.WRITE_ARTICLES, True),
+		'Moderator': (Permission.FOLLOW |
+					  Permission.COMMENT |
+					  Permission.WRITE_ARTICLES|
+					  Permission.MODERATE_COMMENTS, False),
+		'Administrator': (0xff, True)
+		}
+
+		collection = db.get_collection('roles')
+		for k, v in roles.items():
+			result = collection.find_one({'name':k})
+			role = Role(k, v[0], v[1])			
+			
+			if result is not None:
+				result = collection.update_one({'name':k}, {'$set':role.to_dict()})
+			else:
+				collection.insert_one(role.to_dict())
 				
 
 	def to_dict(self):
-		dict_flames = {
+		dict_role = {
 			'name': self.name, 
-			'ID': self.ID,
-			'price': self.price,
-			'picture': self.picture,
-		#	'permission': self.permission,
+			'permission': self.permission,
 			'default': self.default
 		}
-		return dict_flames
+		return dict_role
 
 	def from_dict(self, data):
 		if data is not None:
 			self.name = data['name']
-			self.ID = data['ID']
-			self.price = data['price']
-			self.picture = data['picture']
-		#	self.permission = data['permission']
+			self.permission = data['permission']
 			self.default = data['default']
 			
+
+class Permission:
+	FOLLOW = 0x01
+	COMMENT = 0x02
+	WRITE_ARTICLES = 0x04
+	MODERATE_COMMENTS = 0x08
+	ADMINISTATOR = 0x80
+###
+
+class Item(UserMixin, object):
+   iid = ""
+   iname = ""
+   price = ""
+   req = ""
+   hash_data = ""
+   file = ""
+
+   def __init__(self, iid, iname, price, req, hash_data, file):
+      self.iid=iid
+      self.iname=iname
+      self.price=price
+      self.req=req
+      self.hash_data=hash_data
+      self.file=file
+
+   def item_search(iname):
+      collection = db.get_collection('item')
+      results = collection.find_one({'iname':iname})
+      if results is not None:
+         return None
+      else:
+         return render_template('item.html',item=item)
+
+   def to_dict(self):
+      dict_item = {
+         'iid': self.iid, 
+         'iname':self.iname,
+         'price':self.price,
+         'req':self.req,
+         'file':self.file,
+         'hash_data':self.hash_data         
+      }
+      return dict_item
+
+   def from_dict(self, data):
+      if data is not None:
+         self.iid = data['iid']
+         self.iname = data['iname']
+         self.price=data['price']
+         self.req=data['req']
+         self.hash_data=data['hash_data']
 
