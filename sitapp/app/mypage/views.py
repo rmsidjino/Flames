a7aa867d6f4cf37bb79bf5f7b4857a99d464447b
rmsidjino <21300704@handong.edu>
2019-12-05 17:51:59 +0000

    mypage

--- sitapp/app/mypage/views.py
+++ sitapp/app/mypage/views.py
@@ -27,17 +27,56 @@ def index():
 			flash('Looks like you have changed your name!')
 		session['name'] = form.name.data
 		form.name.data = ''
-
-		'''
-		{file:url_for(main.image', filename=file) for file in fs.list()}
-		'''
-		return redirect(url_for('.index'))
+		return redirect(url_for('.mypage'))
+	try:
+		collection = db.get_collection('users')
+		results = collection.find({'id':current_user.id})
+		collection = db.get_collection('items')
+		collection.update_many({}, {'$set': {'participation': 'no'}})
+		if results != None:
+			collection.update_many({'participation_uid': current_user.id}, {'$set': {'participation': 'yes'}})
+	except AttributeError:
+		pass
 	return render_template('mypage.html',
-							item_list = [i for i in db.get_collection('items').find()],
+							item_list = [i for i in db.get_collection('items').find({'participation_uid':current_user.id})],
 							file_lst = {file:url_for('main.image', filename=file) for file in fs.list()},
 							form=form, name=session.get('name'),
 							known=session.get('known', False),
 							current_time=datetime.utcnow())
+
+@mypage.route('/participation/<userid>/<iid>', methods=['GET'])
+def participation(userid,iid):
+	if current_user.is_authenticated:
+		userid=current_user.id
+		collection = db.get_collection('users')
+		collection.update_one({'id':userid},{'$push':{'participation_iid':iid}})
+		collection = db.get_collection('items')
+		collection.update_one({'iid':iid},{'$push':{'participation_uid':userid}})
+		result=[i for i in collection.find({'iid':iid})]
+		num=str(len(result[0]['participation_uid']))
+		collection.update_one({'iid':iid},{'$set':{'participation_num':num}})
+		collection.update_one({'iid':iid},{'$set':{'participation':'yes'}})
+		return redirect(url_for('.mypage'))
+	else:
+		flash("You must login!!")
+		return render_template('need_login.html')
+
+@mypage.route('/participation_out/<userid>/<iid>', methods=['GET'])
+def participation_out(userid,iid):
+	if current_user.is_authenticated:
+		userid=current_user.id
+		collection = db.get_collection('users')
+		collection.update_one({'id':userid},{'$pull':{'participation_iid':iid}})
+		collection = db.get_collection('items')
+		collection.update_one({'iid':iid},{'$pull':{'participation_uid':userid}})
+		result=[i for i in collection.find({'iid':iid})]
+		num=str(len(result[0]['participation_uid']))
+		collection.update_one({'iid':iid},{'$set':{'participation_num':num}})
+		collection.update_one({'iid':iid},{'$set':{'participation':'no'}})
+		return redirect(url_for('.mypage'))
+	else:
+		flash("You must login!!")
+		return render_template('need_login.html')
 
 @mypage.route('/images/<filename>')
 def image(filename):
